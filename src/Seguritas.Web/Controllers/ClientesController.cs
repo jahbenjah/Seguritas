7dbaf6c3c18eadc16c702a5a8f8cfee2b7f1e8bc
Benjamin Camacho <jahbenjah@hotmail.com>
2020-01-14 14:22:20 -0600

    Se agrega controlador clientes y vista para crear

---
+++ src/Seguritas.Web/Controllers/ClientesController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+
+namespace Seguritas.Web.Controllers
+{
+    public class ClientesController : Controller
+    {
+
+        // GET: Clientes
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Clientes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
